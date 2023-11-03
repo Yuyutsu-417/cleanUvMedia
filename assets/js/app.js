7ee5dd29670c2076838c69c8071aa61cfe4837ba
Yuyutsu-417 <ybamal977@gamil.com>
2023-11-03 11:38:02 +0530

    cleuvmedia

---
+++ assets/js/app.js
@@ -0,0 +1,112 @@
+$('.S2-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll:0,
+    infinite: true,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            dots: true,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+});
+
+$('.S3-slider').slick({
+    infinite: true,
+    slidesToShow: 1.6,
+    slidesToScroll: 1,
+    // responsive: [
+    //   {
+    //     breakpoint: 768,
+    //     settings: {
+    //       arrows: false,
+    //       centerMode: true,
+    //       centerPadding: '40px',
+    //       slidesToShow: 3
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       arrows: false,
+    //       centerMode: true,
+    //       centerPadding: '40px',
+    //       slidesToShow: 1
+    //     }
+    //   }
+    // ]
+});
+
+$('.S7-slider').slick({
+    infinite: true,
+    slidesToScroll: 1,
+    dots: true,
+    slidesToShow: 1.43,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 1.05
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+});
+
+let box = document.querySelectorAll(".acc-box");
+
+box.forEach((row) => {
+    row.addEventListener("click", () => {
+        const x = document.querySelector(".active");
+        row.classList.toggle("active");
+        x && x.classList.remove("active");
+    })
+})
+
+let label = document.querySelector(".label");
+let menu = document.querySelector("#menu");
+let open = 0;
+let aTags = document.querySelectorAll("#menu a");
+
+aTags.forEach((c) => {
+    c.addEventListener("click", () => {
+        if (open != 0) {
+            menu.style.right ='-100%'; 
+            document.body.classList.remove("overflow-y-hidden");
+            label.classList.remove("label-switch");
+            open--;
+        }
+    })
+})
+
+label.addEventListener("click", () => {
+    if (open == 0) {
+        menu.style.right = 0;
+        document.body.classList.add("overflow-y-hidden");
+        label.classList.add("label-switch");
+        open++;
+    }
+
+    else {
+        menu.style.right = '-100%';
+        document.body.classList.remove("overflow-y-hidden");
+        label.classList.remove("label-switch");
+        open--;
+    }
+});
